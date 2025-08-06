f5bdc00fb4a5bd966ef1d472188642fe150fc8d1
husain <Hussain.folath@gmail.com>
2025-08-06 13:42:37 +0300

    finished styling for product new page

--- views/products/New.jsx
+++ views/products/New.jsx
@@ -1,35 +1,44 @@
-const React=require("react")
-const Layout=require("../layouts/Layout")
+const React = require("react")
+const Layout = require("../layouts/Layout")
 
-function NewView(props)
-{
+function NewView(props) {
     const token = props.token
-    const suppliers=props.suppliers
-    return(
+    const suppliers = props.suppliers
+    return (
         <Layout token={token}>
-            <h1>Add new product</h1>
-            <form action={`/products?token=${token}`} method="post">
-            Product name: <input type="text" name="name" placeholder="Enter name here"/> <br />
-            Product description: <input type="text" name="description" placeholder="Enter description here"/><br />
-            Product Image URL: <input type="text" name="image" placeholder="Paste URL here"/><br />
-            Product price: <input type="number" name="price" defaultValue="1"/><br />
-            Product stock: <input type="number" name="stock" defaultValue="1"/><br />
-            Supplier: 
-            <select name="supplier">
-                <option value="">Select a supplier</option>
-                {
-                    suppliers.map(supplier=>{
-                        return(
-                            <option value={`${supplier._id}`}>{`${supplier.name}`}</option>
-                        )
-                    })
-                }
-            </select> <br />
-            <input type="submit" value="Add Product" />
-            </form>
-            <a href={`/products?token=${token}`}>Go back</a>
+            <div id="productContainerNew">
+                <h1>Add new product</h1>
+                <div id="productFormContainerNew">
+                    <form action={`/products?token=${token}`} method="post">
+                    <div id="productFormSubContainerNew">
+                    <div id="">
+                        Product name: <br /><input type="text" name="name" placeholder="Enter name here" />
+                        Product description:<br /> <input type="text" name="description" placeholder="Enter description here" />
+                        Product Image URL:<br /> <input type="text" name="image" placeholder="Paste URL here" />
+                    </div>
+                    <div>
+                        Product price:<br /> <input type="number" name="price" defaultValue="1" />
+                        Product stock:<br /> <input type="number" name="stock" defaultValue="1" />
+                        Supplier:<br />
+                        <select name="supplier">
+                            <option value="">Select a supplier</option>
+                            {
+                                suppliers.map(supplier => {
+                                    return (
+                                        <option value={`${supplier._id}`}>{`${supplier.name}`}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                        </div> <br />
+                        </div>
+                        <input type="submit" value="Add Product" id="productAddBtnNew"/>
+                    </form>
+                </div>
+                <a href={`/products?token=${token}`}><button id="productReturnBtnNew">Return</button></a>
+            </div>
         </Layout>
     )
 }
 
-module.exports=NewView
+module.exports = NewView
